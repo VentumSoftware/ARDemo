ff635ed1cc9d0f6d6e4c8c500370787570994af8
jbnogal@ventum-software.com <jbnogal@ventum-software.com>
2020-09-06 22:54:54 -0300

    pantallas iniciales

---
+++ AR Demo/Assets/ImageClassify/ImageClassify.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageClassify : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        GetComponent<RawImage>().texture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
